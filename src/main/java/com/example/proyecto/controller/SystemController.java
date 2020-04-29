1fe7cb2e5222f90e7b80a12a1bfef2fb922cd4b2
Anthony Rojas <a20145976@pucp.pe>
2020-04-28 21:58:08 -0500

    baseProyecto

---
+++ src/main/java/com/example/proyecto/controller/SystemController.java
@@ -0,0 +1,21 @@
+package com.example.proyecto.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/system")
+public class SystemController {
+
+    @GetMapping("principal")
+    public String listar(){
+        return "base/basePag";
+    }
+
+    @GetMapping(value = {"","iniciosesion"})
+    public String inicioSesion(){
+        return "index";
+    }
+
+}
