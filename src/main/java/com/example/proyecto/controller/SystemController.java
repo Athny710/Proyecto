a75e9b924601fe9fa5cca426e0914d0fce5f7533
JZambrano <jbzambrano@pucp.pe>
2020-05-02 18:05:55 -0500

    Parte Completa, se agregó G-GenReport G-GenReport2 G-EditCategoria G-EditArtesano G-EditComunidad G-RegCategoria cambiarContraseña

--- src/main/java/com/example/proyecto/controller/SystemController.java
+++ src/main/java/com/example/proyecto/controller/SystemController.java
@@ -29,15 +29,31 @@ public class SystemController {
         return "index";
     }
 
+
     @GetMapping("gestorRegCompra")
     public String RegistroCompra(){return "Gestor/G-RegCompra";}
     @GetMapping("gestorEditProdCompra")
     public String EditProdCompra(){return "Gestor/G-EditProdCompra";}
+    @GetMapping("gestorEditComunidad")
+    public String EditComunidad(){return "Gestor/G-EditComunidad";}
+    @GetMapping("gestorEditArtesano")
+    public String EditArtesano(){return "Gestor/G-EditArtesano";}
     @GetMapping("gestorRegistroUsuarioSede")
     public String registroUsuarioSede(){return "Gestor/G-RegistroUsuarioSede";}
+    @GetMapping("gestorRegCategoria")
+    public String RegistroCategoria(){return "Gestor/G-RegCategoria";}
+    @GetMapping("gestorEditCategoria")
+    public String EditCategoria(){return "Gestor/G-EditCategoria";}
     @GetMapping("gestorGestionVentas")
     public String registroVentas(){return "Gestor/G-GestiónVentas";}
+
+    @GetMapping("gestorGenerarReporte")
+    public String reporteVentas(){return "Gestor/G-GenReporte";}
+    @GetMapping("gestorGenerarReporte2")
+    public String reporteVentas2(){return "Gestor/G-GenReporte2";}
     @GetMapping("gestorRegistroSede")
     public String registroSede(){return "Gestor/G-RegistroSede";}
+    @GetMapping("cambiarContrasena")
+    public String cambiarContrasena(){return "cambiarContrasena";}
 
 }
