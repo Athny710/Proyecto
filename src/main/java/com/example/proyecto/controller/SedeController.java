134a9a64f7b17a54c554f4de593623d13d80bb7c
Anthony Rojas <a20145976@pucp.pe>
2020-05-09 13:20:01 -0500

    entidades1

--- src/main/java/com/example/proyecto/controller/SedeController.java
+++ src/main/java/com/example/proyecto/controller/SedeController.java
@@ -1,18 +1,31 @@
 package com.example.proyecto.controller;
 
+import com.example.proyecto.entity.Inventario;
+import com.example.proyecto.repository.InventarioRepository;
+import org.apache.catalina.LifecycleState;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("sede")
+@RequestMapping("/sede")
 public class SedeController {
+
+    @Autowired
+    InventarioRepository inventarioRepository;
+
     @GetMapping("detalleProducto")
     public String detalleProducto(){
         return "UsuarioSede/DetallesProducto";
     }
-    @GetMapping("principal")
-    public String principal(){
+    @GetMapping(value = {"principal", "listar"})
+    public String principal(Model model){
+        List<Inventario> lista = inventarioRepository.findAll();
+        model.addAttribute("listaProductos", lista);
         return "UsuarioSede/Principal";
     }
     @GetMapping("nuevoProducto")
