9e4b68302a3f76e1098e417e7ceb9fb68d08b97a
Anthony Rojas <a20145976@pucp.pe>
2020-05-09 18:08:02 -0500

    listar3

--- src/main/java/com/example/proyecto/controller/SedeController.java
+++ src/main/java/com/example/proyecto/controller/SedeController.java
@@ -1,6 +1,7 @@
 package com.example.proyecto.controller;
 
 import com.example.proyecto.entity.Inventario;
+import com.example.proyecto.repository.ComunidadRepository;
 import com.example.proyecto.repository.InventarioRepository;
 import org.apache.catalina.LifecycleState;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/sede")
@@ -17,12 +20,20 @@ public class SedeController {
 
     @Autowired
     InventarioRepository inventarioRepository;
+    @Autowired
+    ComunidadRepository comunidadRepository;
 
     @GetMapping("detalleProducto")
-    public String detalleProducto(){
-        return "UsuarioSede/DetallesProducto";
+    public String detalleProducto(@RequestParam("id") int id, Model model){
+        Optional<Inventario> opt = inventarioRepository.findById(id);
+        if(opt.isPresent()){
+            Inventario inv =opt.get();
+            model.addAttribute("inventario", inv);
+            return "UsuarioSede/DetallesProducto";
+        }else{
+            return "UsuarioSede/Principal";
+        }
     }
-
 
     @GetMapping("principal")
     public String principal(Model model){
@@ -31,7 +42,8 @@ public class SedeController {
         return "UsuarioSede/Principal";
     }
     @GetMapping("nuevoProducto")
-    public String nuevoProducto(){
+    public String nuevoProducto(Model model){
+        model.addAttribute("listaComunidad", comunidadRepository.findAll());
         return "UsuarioSede/NuevoProducto";
     }
     @GetMapping("editarProducto")
