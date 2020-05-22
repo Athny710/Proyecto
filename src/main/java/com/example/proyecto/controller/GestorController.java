78c9ca254cbfc94e9bb2c175c9722adec1bd3dbd
Julio Carrion <a20161448@pucp.edu.pe>
2020-05-22 16:56:49 -0500

    22/05/2020

--- src/main/java/com/example/proyecto/controller/GestorController.java
+++ src/main/java/com/example/proyecto/controller/GestorController.java
@@ -35,11 +35,9 @@ public class GestorController {
     @Autowired
     InventarioRepository inventarioRepository;
     @Autowired
-<<<<<<< HEAD
     VentaRepository ventaRepository;
-=======
+    @Autowired
     SedeRepository sedeRepository;
->>>>>>> 980768593e760c8a94c82ea5aa5772a9dc778445
 
 
     // ----------------------- ENLACES ---------------------------------
@@ -55,14 +53,8 @@ public class GestorController {
     }
 
     @GetMapping("gestorGestionVentas")
-<<<<<<< HEAD
-    public String registroVentas(){return "G-GestionVentas";}
-=======
-    public String registroVentas() {
-        return "Gestor/G-GestiónVentas";
-    }
+    public String registroVentas(){return "GEstor/G-GestionVentas";}
 
->>>>>>> 980768593e760c8a94c82ea5aa5772a9dc778445
     @GetMapping("gestorResgistroSede")
     public String registroSede() {
         return "Gestor/G-RegistroSede";
