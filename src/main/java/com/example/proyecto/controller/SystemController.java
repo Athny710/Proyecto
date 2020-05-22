88d490d75ea9c596663e05d841970109968b703b
Anthony Rojas <a20145976@pucp.pe>
2020-05-22 16:52:11 -0500

    1

--- src/main/java/com/example/proyecto/controller/SystemController.java
+++ src/main/java/com/example/proyecto/controller/SystemController.java
@@ -1,8 +1,15 @@
 package com.example.proyecto.controller;
 
+
+import com.example.proyecto.entity.Usuarios;
+import com.example.proyecto.repository.UsuarioRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/system")
@@ -12,4 +19,6 @@ public class SystemController {
     public String cambiarContraseña(){
         return "Sistema/S-NuevContra2";
     }
+
+
 }
