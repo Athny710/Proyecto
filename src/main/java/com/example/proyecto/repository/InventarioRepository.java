05a5c51703c3c2b9dee97e743aa6932a1d87fa50
Julio Carrion <a20161448@pucp.edu.pe>
2020-07-06 18:40:43 -0500

    AHHHHHHHHHHHHHHHHHHHHHH

--- src/main/java/com/example/proyecto/repository/InventarioRepository.java
+++ src/main/java/com/example/proyecto/repository/InventarioRepository.java
@@ -19,4 +19,37 @@ public interface InventarioRepository extends JpaRepository<Inventario, Integer>
             "where stock > 0",
             nativeQuery = true)
     List<Inventario> listarStockMayor0();
+
+    @Query(value = "SELECT inventario.* FROM inventario, producto\n" +
+            "where inventario.idProducto = producto.idProducto\n" +
+            "and producto.idComunidad = ?1",
+            nativeQuery = true)
+    List<Inventario> listarPorComunidad(int id);
+
+    @Query(value = "SELECT inventario.* FROM inventario, producto, adquisicion\n" +
+            "where inventario.idProducto = producto.idProducto\n" +
+            "and producto.idAdquisicion = adquisicion.idAdquisicion \n" +
+            "and adquisicion.modalidad =?1",
+            nativeQuery = true)
+    List<Inventario> listarPorAdquisicion(String tipo);
+
+    @Query(value = "SELECT inventario.* FROM inventario, producto\n" +
+            "where inventario.idProducto = producto.idProducto\n" +
+            "and producto.idCategoria = ?1 ;",
+            nativeQuery = true)
+    List<Inventario> listarPorCategoria(int id);
+
+    @Query(value = "SELECT inventario.* FROM inventario, producto, adquisicion\n" +
+            "where inventario.idProducto = producto.idProducto\n" +
+            "and producto.idAdquisicion =adquisicion.idAdquisicion\n" +
+            "and adquisicion.modalidad = ?1 and adquisicion.idArtesano= ?2",
+            nativeQuery = true)
+    List<Inventario> listarPorArtesanoConConsigna(String tipo, int id);
+
+    @Query(value = "SELECT inventario.* FROM inventario, producto\n" +
+            "where inventario.idProducto = producto.idProducto\n" +
+            "and producto.idCategoria =?1 and producto.idComunidad =?2",
+            nativeQuery = true)
+    List<Inventario> listarPorCategoriaYComunidad(int idCate, int idComu);
+
 }
